ab49d575b73d39e6bdd683d21a086955c9437249
Henrique Heiden <henrique.heiden@gmail.com>
2022-04-04 17:21:45 -0300

    |Estrutura da aplicação|

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
+import { GlobalStyle } from "./styles/global";
 
 
 export function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>
+        Hello World
+        <GlobalStyle />
+      </h1>
     </div>
   );
 }
-
-
