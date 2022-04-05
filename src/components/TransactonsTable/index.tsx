1d372d0c3e829fda3c597ae4000052e1d71741fa
Henrique Heiden <henrique.heiden@gmail.com>
2022-04-05 17:33:05 -0300

    |MirageJs| Axios|

--- src/components/TransactonsTable/index.tsx
+++ src/components/TransactonsTable/index.tsx
@@ -1,6 +1,12 @@
+import { useEffect } from "react";
+import { api } from "../../services/api";
 import { Container } from "./styled";
 
 export function TransactonsTable() {
+    useEffect(() => {
+        api.get('transactions')
+            .then(response => console.log(response.data))
+    }, []);
     return (
         <Container>
             <table>
